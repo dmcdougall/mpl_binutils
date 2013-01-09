257a01594cc1addf3548d8ff5f7d6c5f2611e437
Damon McDougall <damon.mcdougall@gmail.com>
2013-01-08 18:42:42 -0600

    Use error codes for failure tests

--- tests/failure_test.py
+++ tests/failure_test.py
@@ -9,30 +9,39 @@ mpl_graph = imp.load_source("mpl-graph", '../mpl-graph')
 
 def failure_setup(test):
     def failure_test():
-        try:
-            sys.stderr = output = io.BytesIO()
-            test()
-            output.close()
-        except SystemExit:
-            assert True
+        # This is just to kill print output
+        sys.stderr = io.BytesIO()
+        test()
     return failure_test
 
 @failure_setup
 def manyoptions_test():
     args = docopt(mpl_graph.usage, argv=['-T', 'png', '-x', '0,1,1,1', 'data.txt'])
-    mpl_graph.produce_plot(args, rcParams)
+    try:
+        mpl_graph.produce_plot(args, rcParams)
+    except SystemExit as e:
+        assert e.code == mpl_graph.ERR_NUM_OPTIONS
 
 @failure_setup
 def floatoption_test():
     args = docopt(mpl_graph.usage, argv=['-T', 'png', '-W', 'asd', 'data.txt'])
-    mpl_graph.produce_plot(args, rcParams)
+    try:
+        mpl_graph.produce_plot(args, rcParams)
+    except SystemExit as e:
+        assert e.code == mpl_graph.ERR_ARG_TYPE
 
 @failure_setup
 def filetype_test():
     args = docopt(mpl_graph.usage, argv=['-T', 'asd', 'data.txt'])
-    mpl_graph.produce_plot(args, rcParams)
+    try:
+        mpl_graph.produce_plot(args, rcParams)
+    except SystemExit as e:
+        assert e.code == mpl_graph.ERR_OUTPUT_TYPE
 
 @failure_setup
 def badfile_test():
     args = docopt(mpl_graph.usage, argv=['-T', 'png', 'badfile.txt'])
-    mpl_graph.produce_plot(args, rcParams)
+    try:
+        mpl_graph.produce_plot(args, rcParams)
+    except SystemExit as e:
+        assert e.code == mpl_graph.ERR_INPUT_FILE
